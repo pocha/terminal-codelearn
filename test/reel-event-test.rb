dd3e5ed8f120273db003a8d1554e4d9d9f3d3c67
pocha <pocha@stalkninja.com>
2013-05-31 14:03:19 +0530

    client script

--- test/reel-event-test.rb
+++ test/reel-event-test.rb
@@ -5,20 +5,24 @@ USER = "pocha"
 
 EM.run {
   client = Faye::Client.new('http://localhost:3000/remote/faye')
+  sending_time = nil
+  receiving_time = nil
 
   subscription = client.subscribe('/output/**') do |message|
-	  puts "#{Time.now} output - #{message.inspect}"
+	  puts "#{Time.now - sending_time} output - #{message.inspect}"
 	  if /(\$|>)\s*\z/.match(message['data']) 
-	  puts "-----------------"
-		  message = {:user => "pocha", :terminal_no => 0, :type => "execute", :command => "whoami"}
-		  puts "#{Time.now} sending input #{message.inspect}"
+	  	  puts "-----------------"
+		  message = {:user => USER, :terminal_no => 0, :type => "execute", :command => "whoami"}
+		  sending_time = Time.now
+		  puts "sending input #{message.inspect}"
 		  client.publish('/input', message)
 	  end 
   end
 
   subscription.callback do
-	  puts "#{Time.now} sending input"
-	  message = {:user => "pocha", :terminal_no => 0}
+	  sending_time = Time.now
+	  puts "sending input"
+	  message = {:user => USER, :terminal_no => 0}
 	  client.publish('/input', message.merge({:type => "create"}))
 	  #client.publish('/input', message.merge({:type => "destroy"}))
   end
