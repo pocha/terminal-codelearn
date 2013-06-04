dab52e8903d1607ce56763023171848ebc184689
dereXeus <aviral12028@gmail.com>
2013-06-03 20:30:01 -0700

    added single user terminal

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 var http = require('http');
 var sockjs = require('sockjs');
+var pty = require('pty.js');
 
 var echo = sockjs.createServer();
 echo.on('connection', function(conn) {
-    conn.on('data',function(message) {
-      conn.write('Hi from server');
+		
+		var term = pty.spawn('bash', [], {
+			name: 'xterm-color',
+			cols: 80,
+			rows: 30,
+			cwd: process.env.HOME,
+			env: process.env
+		});
+    
+		term.on('data', function(data) {
+  		conn.write(data);
+		});
+
+		conn.on('data',function(message) {
+			console.log('Input :' + message);      
+			term.write(message+'\r');
     });
-    conn.on('close', function() {});
+    
+		conn.on('close', function() {
+			term.kill();					
+		});
 });
 
 var server = http.createServer();
