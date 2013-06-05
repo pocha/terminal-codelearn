a41f3cbb73f88ff58fda24b808b721ddfd0b4b3f
dereXeus <aviral12028@gmail.com>
2013-06-04 19:43:41 -0700

    Client can now be accessed at the server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-var http = require('http');
-var sockjs = require('sockjs');
-var pty = require('pty.js');
+var http = require('http'),
+	sockjs = require('sockjs'),
+	pty = require('pty.js'),
+	fs = require('fs'),
+	fileStatic = require('node-static');
 
 var echo = sockjs.createServer();
 echo.on('connection', function(conn) {
@@ -12,21 +14,39 @@ echo.on('connection', function(conn) {
 			cwd: process.env.HOME,
 			env: process.env
 		});
-    
+	    
+		console.log("Process started: " + term.pid);		
+
 		term.on('data', function(data) {
   		conn.write(data);
 		});
+		
+		term.on('exit', function(code,signal){
+			console.log("Process "+term.pid+" exited with code: "+code+" and signal "+signal);
+		});
 
 		conn.on('data',function(message) {
-			console.log('Input :' + message);      
 			term.write(message+'\r');
     });
     
 		conn.on('close', function() {
-			term.kill();					
+			term.write('exit\r');					
 		});
 });
+var fileServer = new fileStatic.Server();
 
-var server = http.createServer();
+var server = http.createServer(function (request, response) {
+	fileServer.serve(request, response, function (err, res) {
+		if (err) {
+				console.error("> Error serving " + request.url + " - " + err.message);
+				response.writeHead(err.status, err.headers);
+				response.end();
+		} else { 
+				console.log("> " + request.url + " - " + res.message);
+		}
+	});
+
+});
+
 echo.installHandlers(server, {prefix:'/echo'});
 server.listen(3000, '0.0.0.0')
