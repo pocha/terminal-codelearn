b5a39caed3685c4a8158aaf69978d45af0e876c8
dereXeus <aviral12028@gmail.com>
2013-06-25 09:18:21 -0700

    Added JsDOM and jQuery to Mocha tests

--- public/assets/terminal-client.js
+++ public/assets/terminal-client.js
@@ -1,47 +1,123 @@
-function conn(host, fn_onopen, fn_onmessage, fn_onclose){
+$('#execute').attr("disabled",true);
 
-	socket = new SockJS('http://'+host+'/echo');
+var Client = function(){
+	socket = new SockJS(SOCKETURL)
 
-	socket.onopen = function() {	
-		console.log('open');
-		fn_onopen();
-		//$('#output').html('');				
-		//$('#execute').button({disabled: false});
-	};
+	socket.onopen = function(){
+		$('#output').html('');
+	}
 
-	socket.onmessage = function(e) {				
-		//console.log(e.data);
-		fn_onmessage();
-		/*$('#output').append(colorReplace(e.data));
-		pre.scrollTop(pre.prop('scrollHeight'));
+	socket.onmessage = function(e){
+		$('#output').append(colorReplace(e.data));
+		$('#output').scrollTop($('#output').prop('scrollHeight'));
 
 		var end_of_output = /(\$|>)\s*$/;
 
 		if(end_of_output.test(e.data)){		
-			$('#execute').button({disabled: false});	
-		};*/
+			$('#execute').attr("disabled",false);	
+		};
+	}
+
+	socket.onclose = function(){
+		$('#execute').attr("disabled",true);
+	}
 
 
-	};
-
-	socket.onclose = function() {
-		console.log('close');
-		fn_onclose();
-		//$('#execute').button({disabled: true});
-	};		
-
-	this.on_execute = function(command) {
+	socket.execute = function(command) {
 		socket.send(command+'\r');
 	}
-	this.on_kill = function() {
+
+	socket.kill = function() {
 		socket.send(String.fromCharCode(3));
-	}
-	this.on_reset = function() {
+	}	
+
+	socket.reset = function() {
 		socket.close()
-		socket = new SockJS('http://'+host+'/echo');
-	}
+		socket = new Client();
+	};
 
-	return socket;
+	return socket;	
+}
 
-};
+socket = new Client();
+
+$("#myForm").submit(function(){
+	if($('#execute').is(':disabled') == false){
+		$('#execute').click();
+	};
+});		
+
+//Buttons
+	$("#execute").click(function(){
+		command = $("input[name='command']").val();			
+		socket.execute(command);
+		$("input[name='command']").val('');
+		$('#execute').attr("disabled",true);
+	});
+
+	$("#kill").click(function(){
+		socket.kill();
+		$('#execute').attr("disabled",true);
+	});
+
+	$("#reset").click(function(){
+		socket.reset();
+		$('#execute').attr("disabled",true);
+	});
+
+
+function colorReplace(input) {
+
+	var span = "<span style='";
+		var color = "color: ";
+		var bold = " font-weight: bold;";
+		var underline = " text-decoration: underline;";
+		var span_fin = "'>";			
+		var span_end = "</span>";
+	var reset = /\033\[0+m/;
+
+	input = input.replace(reset,"<span>");
+
+		var replaceColors = {
+			"31" : "red;",
+			"01;31" : "red;" + bold,
+
+			"32" : "green;",
+			"01;32" : "green;" + bold,
+
+			"33" : "yellow;",
+			"01;33" : "yellow;" + bold,
+
+			"34" : "blue;",
+			"01;34" : "blue;" + bold,
+
+			"35" : "purple;",
+			"01;35" : "purple;" + bold,
+
+			"36" : "cyan;",
+			"01;36" : "cyan;" +bold,
+			"04;36" : "cyan;" +underline,
+
+			"37" : "white;",
+			"01;37" : "white;" +bold,
+
+			"01;30" : bold,
+			"01" : bold
+
+		};
+
+
+		for( k in replaceColors )
+		{
+			var re = new RegExp( "\\033\\[" + k + "m", "g" );
+			var re_str  = span_end  + span + color + replaceColors[ k ] + span_fin;
+			input = input.replace( re,re_str);
+		};
+
+		input = input.replace(/\033\[0+m/g,span_end);			
+
+		return input;
+}
+
+
 
--- test/test.js
+++ test/test.js
@@ -1,127 +1,122 @@
 var Terminal = require('../lib/Terminal.js');
 var Config = require('../lib/Config.js');
-var exec = require('child_process').exec;
+var jsdom = require('jsdom');
 var async = require('async');
-var WebSocket = require('ws');
+var jQuery = require('jQuery');
+var fs = require('fs');
+var exec = require('child_process').exec;
 
+Terminal.listen();
 
-var buff = ""
-var buff1 = ""
+var htmlFile = fs.readFileSync("./public/client.html").toString();
 
-var  Client = function(done){
+	SockJS = require('ws');
+	SOCKETURL = "ws://"+Config.host+":"+Config.port+Config.prefix+"/websocket"
 
-	socket =  new WebSocket("ws://"+Config.host+":"+Config.port+Config.prefix+"/websocket");
-			
-	socket.on('open',function(){
-		buff="";			
-		done();	
-	});
+window = jsdom.jsdom(htmlFile).createWindow();
 
-	socket.on('message',function(data,flags){
-		buff += data;
-	});
-	
-	return socket;
-}
+$ = jQuery.create(window);
+
+require("../public/assets/terminal-client.js");
+
 
 describe('Connection',function(){
 
-	var c1,c2;
-
 	before(function(done){
-		Terminal.listen(done);
-	});
-
-	before(function(done){
-		c1=new Client(done);
+		check(done);
 	});
 
 	it("it should return '"+process.env.USER+"' when I send 'whoami'",function(done){
-		c1.send('whoami\r');
+		$("input[name='command']").val('whoami');
+		$('#execute').click();
 		async.series([
 			function(callback){
 				check(callback);			
 			},
 			function(callback){				
-				result = buff.split("\n")[2];
-				result.should.match(new RegExp(process.env.USER));
-				buff ="";				
-				callback();			
+				result = $('#output').html().split("\n")[1];
+				result.should.match(new RegExp(process.env.USER));				
+				callback();		
 			}
 		],
 		function(){
+			$('#output').html('');
 			done();	
 		});
 	});
+	
+	it("it should stop the command execution when I press kill",function(done){
+		var buff;
 
-	it("it should stop the command execution when I send unicode for Ctrl+C ",function(done){
 		async.series([
 			function(callback){
-				c1.send('sleep 100\r');
+				$("input[name='command']").val('sleep 100');
+				$('#execute').click();
 				setTimeout(callback,50);	
 			},
 			function(callback){
 				exec("ps -ef | grep 'sleep 100' | awk '{print $8"+'"\t"'+"$9}'",function(error,stdout,stderr){
-					buff1 += stdout;
+					buff += stdout;
 					callback();		
 				});			
 			},
 			function(callback){				
-				result = buff1.split("\n")[0];
-				buff1 = "";				
+				result = buff.split("\n")[0];
+				buff = "";				
 				result.should.match(/sleep\s*100/);				
-				c1.send(String.fromCharCode(3));
+				$('#kill').click();
 				exec("ps -ef | grep 'sleep 100' | awk '{print $8"+'"\t"'+"$9}'",function(error,stdout,stderr){
-					buff1 += stdout;
+					buff += stdout;
 					callback();		
 				});			
 			},
 			function(callback){				
-				result = buff1.split("\n")[0];
-				buff1 = "";				
+				result = buff.split("\n")[0];
+				buff = "";				
 				result.should.not.match(/sleep\s*100/);
 				callback();
 			}
 		],
 		function(){
+			$('#output').html('');
 			done();	
 		});
 		
 	});
-	
-	it("it should return a different pid on a new connection ",function(done){
+
+	it("it should return a different pid when I press reset",function(done){
 
 		var pid1, pid2;
 
 		async.series([
 			function(callback){
-				buff=""
-				c1.send('echo $$\r');
+				$("input[name='command']").val('echo $$');
+				$('#execute').click();
 				check(callback);	
 			},
 			function(callback){
-				pid1 = parseInt(buff.split('\n')[1]);
-				c1.close();
-				c2 = new Client(callback);
-		
+				pid1 = parseInt($('#output').html().split('\n')[1]);
+				$('#reset').click();
+				callback();
 			},
 			function(callback){
 				check(callback)			
 			},
-			function(callback){				
-				buff="";
-				c2.send('echo $$\r');
-				check(callback);	
+			function(callback){
+				$('#output').html('');
+				$("input[name='command']").val('echo $$');
+				$('#execute').click();
+				check(callback);
 			},
 			function(callback){
-				pid2 = parseInt(buff.split('\n')[1]);
-				
-				pid1.should.not.equal(pid2);
-				
+				pid2 = parseInt($('#output').html().split('\n')[1]);
+	
+				pid1.should.not.equal(pid2);				
 				callback();
 			}
 		],
 		function(){
+			$('#output').html('');
 			done();	
 		});
 		
@@ -130,22 +125,15 @@ describe('Connection',function(){
 	after(function(){
 		Terminal.close();	
 	});
-
 });
 
-
 function check(callback){
-	
-	var end_of_output = /(\$|>)\s*$/;	
-	
-	//console.log(buff);
-	state = end_of_output.test(buff);
-
-	if(!state){
+	if($('#execute').is(':disabled')){
 		setTimeout(function(){
 			check(callback);
-		},25);
+		},500);				
 	} else {	
 		callback();
 	}
 };
+
