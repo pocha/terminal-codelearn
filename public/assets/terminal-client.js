53b6de998ee2f52351407cb6b0849e1e2af6924c
pocha <pocha@stalkninja.com>
2013-08-01 01:25:35 +0530

    added support for command autocomplete through tab & up/down arrow key for navigation. Tests would fail for now as the form & 'Go' button is removed.

--- public/assets/terminal-client.js
+++ public/assets/terminal-client.js
@@ -1,31 +1,25 @@
-$('#execute').attr("disabled",true);
-
 var state = false;
 
 var filter = new Filter();
+
+var history = new Array()
+var ptr = 0
 
 var Client = function(){
 
 	var socket = new SockJS(SOCKET_URL)
 
 	socket.onopen = function(){
+		initializeOutput()
 		socket.send(JSON.stringify({user: USERNAME, signature: SIGNATURE})); //Send a username here to make the pty spawn as that user
-		$('#output').html('');
 		state = true;
 	}
 
 	socket.onmessage = function(e){
 		appendOutput(filter.toHtml(e.data));
-		
-		//var end_of_output = /(\$|>)\s*$/;
-
-		//if(end_of_output.test(e.data)){		
-			$('#execute').attr("disabled",false);	
-		//};
 	}
 
 	socket.onclose = socket.onerror = function(){
-		$('#execute').attr("disabled",true);
 		if(state)		
 			appendOutput("\nConnection to server closed.... Please click 'Reset' to reconnect");
 		else
@@ -40,49 +34,94 @@ var Client = function(){
 
 	socket.kill = function() {
 		socket.send(String.fromCharCode(3));
-	}	
+	}
+	
+	socket.sendData = function(character) {
+		console.log(character)
+		socket.send($("input[name='command']").val() + character);
+	}
 
 	socket.reset = function() {
 		socket.close();
-		$('#output').html('');
-		appendOutput("\nConnecting...");
+		initializeOutput();
 		client1 = new Client();
+		ptr = history.length
 	};
 
 	return socket;	
 }
 
 client1 = new Client();
-
-$("#myForm").submit(function(){
-	if($('#execute').is(':disabled') == false){
-		$('#execute').click();
-	};
-});		
+		
 
 //Buttons
-	$("#execute").click(function(){
-		command = $("input[name='command']").val();			
-		client1.execute(command);
-		$("input[name='command']").val('');
-		$('#execute').attr("disabled",true);
-	});
 
 	$("#kill").click(function(){
 		client1.kill();
-		$('#execute').attr("disabled",true);
 	});
 
 	$("#reset").click(function(){
 		client1.reset();
-		$('#execute').attr("disabled",true);
 	});
+	
+	
+
+function sendData(character) {
+	client1.sendData(character);
+	$("input[name='command']").val("")
+}
+
 
 timerFired = function (){
 	client1.close();
 }
 
 function appendOutput(data){
-	$('#output').append(data);
+	//$('#output').append(data);
+	//console.log(data)
+	$("#output input").before(data)
 	$('#output').scrollTop($('#output').prop('scrollHeight'));
+}
+
+function initializeOutput() {
+	$("input[name='command']").val("")
+	appendOutput("\nConnecting...");
+	$("#output").html('<input type="text" name="command" class="input" autocomplete="off">')
+
+	$("input[name='command']").keydown(function(event) {
+		switch(event.which) {
+			case 13: //enter key
+				event.preventDefault();
+				history[ptr] = $("input[name='command']").val()
+				if (++ptr > history.length) ptr = history.length
+				//console.log(history + " " + ptr)
+				sendData('\r')
+			break;
+			case 38: //up key
+				event.preventDefault();
+				if (--ptr < -1) ptr = -1
+				$("input[name='command']").val(history[ptr])
+				//console.log(history + " " + ptr)
+				//sendData('\x1b[A')
+
+			break;
+			case 40: //down key
+				event.preventDefault();
+				if (++ptr > history.length) ptr = history.length
+				$("input[name='command']").val(history[ptr])
+				//console.log(history + " " + ptr)
+				//sendData('\x1b[B')
+			break;
+			case 9: //tab key
+				event.preventDefault();
+				sendData('\t')
+			break;
+			case 67: //checking control C
+				if (event.ctrlKey) {
+					event.preventDefault();
+					client1.kill()
+				}
+			break;
+		}
+	})
 }
